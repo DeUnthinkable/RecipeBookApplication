188358a5b81d9d1d7afaa3b88ff4edab0b3d8b13
Mohamed_Aljak <m.aljak101@gmail.com>
2023-07-27 19:10:58 -0400

    Condensed the 3 getters and 3 setters for macronutrients in NutritionFacts.java into 1 getter and 1 setter.

--- src/main/java/com/example/domain/NutritionFacts.java
+++ src/main/java/com/example/domain/NutritionFacts.java
@@ -3,34 +3,25 @@ package com.example.domain;
 import java.util.HashMap;
 import java.util.Map;
 
-public class NutritionFacts {
+public class NutritionFacts
+{
     private final Map<Macronutrient, Integer> macronutrientCountMap;
 
-    public NutritionFacts() {
+    public NutritionFacts()
+    {
         this.macronutrientCountMap = new HashMap<>();
     }
-    public void setProteinValue(int value){
-        if(value > 0) {
-            this.macronutrientCountMap.put(Macronutrient.PROTEIN, value);
+
+    public void setMacronutrientValue(Macronutrient macronutrient, int value)
+    {
+        if (value > 0)
+        {
+            this.macronutrientCountMap.put(macronutrient, value);
         }
     }
-    public int getProteinValue(){
-        return this.macronutrientCountMap.getOrDefault(Macronutrient.PROTEIN, 0);
-    }
-    public void setFatValue(int value){
-        if(value > 0) {
-            this.macronutrientCountMap.put(Macronutrient.FAT, value);
-        }
-    }
-    public int getFatValue(){
-        return this.macronutrientCountMap.getOrDefault(Macronutrient.FAT, 0);
-    }
-    public void setCarbValue(int value){
-        if(value > 0) {
-            this.macronutrientCountMap.put(Macronutrient.CARBOHYDRATE, value);
-        }
-    }
-    public int getCarbValue(){
-        return this.macronutrientCountMap.getOrDefault(Macronutrient.CARBOHYDRATE, 0);
+
+    public int getMacronutrientValue(Macronutrient macronutrient)
+    {
+        return this.macronutrientCountMap.getOrDefault(macronutrient, 0);
     }
 }
