fe774e89270d182da45593cfbccfcb3e51486d95
Mohamed_Aljak <m.aljak101@gmail.com>
2023-08-02 19:19:14 -0400

    Made RecipeList and IngredientList final. Added variables ingredientsFilePath and RecipeFilePath and their respective getters and setters.

--- src/main/java/com/example/logic/StartViewController.java
+++ src/main/java/com/example/logic/StartViewController.java
@@ -1,7 +1,9 @@
 package com.example.logic;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class StartViewController
 {
