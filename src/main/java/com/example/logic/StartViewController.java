3b8bb7be563788f94da2283a6c48fb7847fb4abb
Mohamed_Aljak <m.aljak101@gmail.com>
2023-08-03 19:00:17 -0400

    Added methods updateData and createRecipeBox. Modified addRecipeButton method to instead create hboxes and also add data to appDataReadWriteStore.

--- src/main/java/com/example/logic/StartViewController.java
+++ src/main/java/com/example/logic/StartViewController.java
@@ -1,8 +1,11 @@
 package com.example.logic;
 
+import com.example.domain.Recipe;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class StartViewController
@@ -16,16 +19,57 @@ public class StartViewController
 
     public void initData(AppDataReadWriteStore appDataReadWriteStore){
         this.appDataReadWriteStore = appDataReadWriteStore;
+
+        //testing code
+        for(int i = 0; i < 3; i++){
+            addRecipeButton();
+        }
     }
 
     @FXML
     protected  void addRecipeButton(){
         //Check to see if column is already too full to add another recipe into it.
         if(leftRecipesButtonsColumn.getChildren().size() < 15){
-            leftRecipesButtonsColumn.getChildren().add(new Button(""));
+            HBox hBox = createRecipeBox();
+
+            leftRecipesButtonsColumn.getChildren().add(hBox);
+            appDataReadWriteStore.getRecipeList().addRecipe(new Recipe(""));
+
         } else if (rightRecipesButtonsColumn.getChildren().size() < 15)
         {
-            rightRecipesButtonsColumn.getChildren().add(new Button(""));
+            HBox hBox = createRecipeBox();
+
+            rightRecipesButtonsColumn.getChildren().add(hBox);
+            appDataReadWriteStore.getRecipeList().addRecipe(new Recipe(""));
         }
+        updateData();
     }
+    private void updateData(){
+        appDataReadWriteStore.writeIngredientsToFile();
+        appDataReadWriteStore.writeRecipesToFile();
+    }
+
+    public HBox createRecipeBox(){
+        HBox hBox = new HBox();
+        hBox.setSpacing(5);
+
+        TextField textField = new TextField();
+        textField.setPrefWidth(240);
+        textField.setPromptText("Recipe name:");
+        hBox.getChildren().add(textField);
+
+        Button button = new Button("open");
+        button.setOpacity(0);
+        hBox.getChildren().add(button);
+
+        hBox.setOnMouseEntered(event -> {
+            hBox.getChildren().get(1).setOpacity(1);
+        });
+        hBox.setOnMouseExited(event -> {
+            hBox.getChildren().get(1).setOpacity(0);
+        });
+
+        return hBox;
+    }
+
 }
