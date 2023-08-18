f13953cd03a9d2f1065afee453d1a79f254785b0
Mohamed_Aljak <m.aljak101@gmail.com>
2023-08-18 14:00:39 -0400

    Fixed method setRecipeFilePath in AppDataReadWriteStore.java.

--- src/main/java/com/example/logic/AppDataReadWriteStore.java
+++ src/main/java/com/example/logic/AppDataReadWriteStore.java
@@ -58,7 +58,7 @@ public class AppDataReadWriteStore
 
     public void setRecipeFilePath(String recipeFilePath)
     {
-        recipeFilePath = recipeFilePath;
+        this.recipeFilePath = recipeFilePath;
     }
 
 
