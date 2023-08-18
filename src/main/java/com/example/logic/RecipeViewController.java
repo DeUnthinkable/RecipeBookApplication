431b5112f167e0d4354672aab80bf55402393e31
Mohamed_Aljak <m.aljak101@gmail.com>
2023-08-18 17:13:23 -0400

    Mofidied recipe-view.fxml and its controller.

--- src/main/java/com/example/logic/RecipeViewController.java
+++ src/main/java/com/example/logic/RecipeViewController.java
@@ -3,19 +3,36 @@ package com.example.logic;
 import com.example.domain.Recipe;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 
 public class RecipeViewController
 {
     private Recipe recipe;
     @FXML
-    private Button button;
+    private TextArea description;
+    @FXML
+    private Button backToStartViewButton;
+    @FXML
+    private Label recipeName;
+
 
     public void initData(Recipe recipe){
         this.recipe = recipe;
+
+        this.recipeName.setText(recipe.getRecipeName());
+        this.description.setText(recipe.getDescription());
+
+        handlers();
     }
 
-    public void onButtonClick(){
-        button.setText(recipe.getRecipeName());
+    public void handlers(){
+        this.backToStartViewButton.setOnAction(event -> SceneController.switchToStartView(event));
+
+        this.description.textProperty().addListener(event -> this.recipe.setDescription(this.description.getText()));
     }
+
+
+
 }
