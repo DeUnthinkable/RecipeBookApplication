c8276509826c9f3bd8ce067054ddc3dfcc929b1a
Mohamed_Aljak <m.aljak101@gmail.com>
2023-09-23 18:53:00 -0400

    Modified RecipeBookApplication.java's start method to initialize AppDataReadWrite

--- src/main/java/com/example/logic/RecipeBookApplication.java
+++ src/main/java/com/example/logic/RecipeBookApplication.java
@@ -3,6 +3,8 @@ package com.example.logic;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class RecipeBookApplication extends Application
 {
     public static void main(String[] args)
@@ -13,6 +15,11 @@ public class RecipeBookApplication extends Application
     @Override
     public void start(Stage stage)
     {
+        //Initiate which files to read data from and write data into
+        File ingredientListFile = new File("Ingredients.csv");
+        File recipeListFile = new File("Recipes.csv");
+        AppDataReadWrite.init(ingredientListFile, recipeListFile);
+
         SceneController.switchToStartView(stage);
     }
 }
